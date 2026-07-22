1877e8f6f70de639c9deffbcd718d4ad3649d9a3
Deimer Hernandez <deimerhdz21@gmail.com>
2026-07-22 08:55:07 -0500

    feat(email): implement email sending functionality using HTTP service

--- app/celery_task.py
+++ app/celery_task.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-from app.core.mail import send_email as send_email_resend, create_message
+from app.core.mail import send_email, create_message
 
 c_app = Celery("pos")
 
@@ -10,5 +10,5 @@ c_app.config_from_object("app.core.config")
 @c_app.task
 def send_email_task(recipients: list[str], subject: str, body: str):
     message = create_message(recipients=recipients, subject=subject, body=body)
-    send_email_resend(message)
+    send_email(message)
     print("Email sent")
